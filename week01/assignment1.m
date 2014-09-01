5fc562245ef696a22696ec2fb21ae07905439ab5
Martin Jørgensen <martinnjorgensen@gmail.com>
2014-09-01 11:19:45 +0200

    IEize

---
+++ week01/assignment1.m
@@ -0,0 +1,8 @@
+% Runs all sub assignments one after another.
+% Written by: Martin Jørgensen, tzk173
+
+% Add folder where images are stored.
+addpath('images');
+
+% Run part 1.1
+run('./p_1_1');
---
+++ week01/p_1_1.m
@@ -0,0 +1,5 @@
+% Solution for part 1.1 of the Assignment.
+% Written by: Martin Jørgensen, tzk173
+
+clear all;
+
